20618006d7f6972f57e247a04ff68122f82a4c1a
hogelog <konbu.komuro@gmail.com>
2018-10-15 00:14:58 +0900

    Build with mrubyc

--- source/main.cpp
+++ source/main.cpp
@@ -1,10 +1,26 @@
 #include "MicroBit.h"
 
-MicroBit uBit;
+#define MRBC_SRC_CONSOLE_H_
+void console_printf(const char *fstr, ...);
+#include "mrubyc.h"
 
-int main()
-{
-    uBit.init();
-    uBit.display.scroll("HELLO WORLD! :)");
-    release_fiber();
+#define MRUBY_MEMORY_SIZE (1024 * 10)
+
+MicroBit uBit;
+static uint8_t memory_pool[MRUBY_MEMORY_SIZE];
+
+struct VM * init_mruby() {
+  //mrbc_init_alloc(memory_pool, MRUBY_MEMORY_SIZE);
+  //mrbc_init_global();
+  //mrbc_init_class();
+  return mrbc_vm_open(NULL);
+}
+
+int main() {
+  uBit.init();
+
+  init_mruby();
+
+  uBit.display.scroll("HELLO WORLD! :)");
+  release_fiber();
 }
